145b9010e8782c904bf071fa169994fd38848dac
SsamWee <rpbertofernandezsepulveda@gmail.com>
2023-10-26 16:54:43 +0200

    Register done

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -41,7 +41,7 @@
                             <div class="content mb-3 d-flex mx-auto">
                                 <div class="checkbox mb-3 ms-2 mx-auto">
                                     <input type="checkbox" class="form-check-input mx-1" name="checkbox" id="checkbox">
-                                    <label for="checkbox" class="form-check-label">Remember me</label>
+                                    <label for="checkbox">Remember me</label>
                                 </div>
                                 <div class="pass-link text-center mx-auto">
                                     <a type="button" @click="changeRoute('forgot')">Forget password?</a>
@@ -98,14 +98,22 @@
 
             <div class="row h-100">
 
-                <div class="col-lg-5 h-100">
+                <div class="col-lg-5 p-3 d-flex flex-column align-items-center justify-content-end h-100">
 
-                    <h2 class="text-center">Welcome Back!</h2>
+                    <div class="text-center mx-auto">
+                        <h2 class="text-center">Welcome Back!</h2>
+                    </div>
 
-                    <p>Hey, Enter your details to login to your account</p>
+                    <div class="text-center mx-auto">
+                        <p>Hey, Enter your details to login to your account</p>
+                    </div>
 
-                    <button type="button" id="startAnimationButtonRegister" class="btn btn-primary"
-                        @click="changeRoute('login')">Sign in</button>
+
+                        <div class="text-center mx-auto">
+                            <button type="button" id="startAnimationButtonRegister" class="custom-button" @click="changeRoute('login')">Sign in</button>
+                    </div>
+
+                    <div class="mx-auto" ref="animationTarget2"></div>
 
 
                 </div>
@@ -116,40 +124,50 @@
 
                         <form action="#">
 
-                            <h2 class="text-center mb-3">Create Account</h2>
-                            <div class="social-container">
+                            <h2 class="text-center mt-1 mb-3">Create Account</h2>
+
+                            <div class="social-container mb-2">
                                 <span href="#" class="social"><font-awesome-icon :icon="['fab', 'facebook-f']" /></span>
                                 <span href="#" class="social"><font-awesome-icon :icon="['fab', 'google-plus-g']" /></span>
                                 <span href="#" class="social"><font-awesome-icon :icon="['fab', 'twitter']" /></span>
                             </div>
-                            <span>or use your email for registration:</span>
+                            <div class="text-center">
+                                <span>or use your email:</span>
+                            </div>
+                            
 
-                            <div class="mb-3 d-flex">
+                            <div class="mb-2 d-flex custom-input">
                                 <span class="input-group-text"
                                     style="background: transparent; border: transparent;"><font-awesome-icon
                                         :icon="['fas', 'user']" /></span>
-                                <input type="text" class="form-control" placeholder="Name">
+                                <input type="text" class="custom-form-control" placeholder="Name">
                             </div>
 
-                            <div class="mb-3 d-flex">
+                            <div class="mb-2 d-flex custom-input">
                                 <span class="input-group-text"
                                     style="background: transparent; border: transparent;"><font-awesome-icon
                                         :icon="['fas', 'envelope']" /></span>
-                                <input type="email" class="form-control" placeholder="Email">
+                                <input type="email" class="custom-form-control" placeholder="Email">
                             </div>
-
-                            <div class="mb-3 d-flex">
+                            <div class="mb-3 d-flex custom-input">
                                 <span class="input-group-text"
                                     style="background: transparent; border: transparent;"><font-awesome-icon
                                         :icon="['fas', 'lock']" /></span>
-                                <input type="password" class="form-control" placeholder="Password"
-                                    style="background-color: rgb(233, 233, 233); border: transparent;">
+                                <input type="password" class="custom-form-control" placeholder="Password">
                             </div>
 
+                            <div class="content d-flex mx-auto">
+                                <div class="checkbox mb-3 ms-2 mx-auto">
+                                    <input type="checkbox" class="form-check-input mx-1" name="checkbox" id="checkbox">
+                                    <label for="checkbox">I agree to the <a class="link-offset-2 link-underline link-underline-opacity-0 link-secondary" href="#">social web</a> terms</label>
+                                </div>
+                            
+                            </div>
 
+                            <div class="text-center mb-2">
+                                <button class="custom-button" type="submit">Sign up</button>
+                            </div>
 
-
-                            <button type="submit">Sign up</button>
                         </form>
 
                     </div>
@@ -204,9 +222,11 @@ import { ref, onMounted, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import lottie from 'lottie-web';
 
-import animacion1 from '../assets/animation1.json';
+import animacion1 from '../assets/animation2.json';
+import animacion2 from '../assets/animation1.json';
 
 const animationTarget = ref(null);
+const animationTarget2 = ref(null);
 
 
 const view = ref('login');
@@ -226,26 +246,28 @@ onMounted(() => {
         view.value = currentRoute.name;
     }
 
-    // Cargar y reproducir la animación al inicio
-    lottie.loadAnimation({
-        container: animationTarget.value,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        animationData: animacion1,
-    });
+    // Cargar y reproducir las animaciones
+    loadAnimation(animationTarget, animacion1);
+    loadAnimation(animationTarget2, animacion2);
 
 });
 
-// Vuelve a cargar y reproducir la animación cuando 'view' cambia
-watch(view, () => {
+function loadAnimation(target, animationData) {
     lottie.loadAnimation({
-        container: animationTarget.value,
+        container: target.value,
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        animationData: animacion1,
+        animationData: animationData,
     });
+}
+
+// Vuelve a cargar y reproducir la animación cuando 'view' cambia
+watch(view, () => {
+
+    
+    loadAnimation(animationTarget, animacion1);
+    loadAnimation(animationTarget2, animacion2);
 });
 
 
@@ -316,6 +338,17 @@ watch(view, () => {
     border: 2px solid #ccc;
     color: #fff; /* Cambia el color del texto al pasar el cursor */
   }
+}
+
+.form-check-input[type=checkbox] {
+    border: 1.5px solid #525252;
+    box-shadow: none;
+}
+.form-check-input[type=checkbox]:checked {
+    background-color: rgb(74, 104, 161);
+}
+.form-check-input[type=checkbox]:hover {
+    box-shadow: 0 0 5px rgb(74, 104, 161);
 }
 
 .pass-link {
